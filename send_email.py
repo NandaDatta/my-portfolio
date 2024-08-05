114268b198fc1630b720e8b675b82951465c5df2
nanda <nandadatta45@gmail.com>
2024-08-05 18:32:50 +0530

    send email by using form data

--- send_email.py
+++ send_email.py
@@ -1,21 +1,17 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "nandadattaburi2003@gmail.com"
-password = "lnff sfql yzin nxsn"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "nandadatta45@gmail.com"
+    username = "nandadattaburi2003@gmail.com"
+    password = "lnff sfql yzin nxsn"
 
-my_context = ssl.create_default_context()
+    receiver = "nandadatta45@gmail.com"
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
+    my_context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
