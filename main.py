e303d914923a62a7b90e2d8e3d66aec2a87fb3af
nanda <nandadatta45@gmail.com>
2024-08-05 13:43:43 +0530

    render the app images, description, links

--- main.py
+++ main.py
@@ -27,16 +27,22 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(text1)
 
-col3, col4 = st.columns(2)
+col3, space_col,  col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[0:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
